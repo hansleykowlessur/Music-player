6cc9ef26add64987f51c363f3897c290cf0ae401
Hansley <hansleykowlessur1998@gmail.com>
2021-11-14 14:05:57 +0400

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+
+import '../styles/App.css';
+import Musicplayer from './Musicplayer';
+import Sidebar from './Sidebar';
+
+function App() {
+  return (
+    <>
+      <Sidebar />
+      <Musicplayer />
+    </>
+  );
+}
+
+export default App;
